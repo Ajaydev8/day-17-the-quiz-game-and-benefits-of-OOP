0f8c63259ac19393775284c0ebba8bd1bec62122
Ajaydev8 <131373049+Ajaydev8@users.noreply.github.com>
2023-09-06 13:51:48 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# A class is just a blueprint to create a desirable object.
+# in this lesson we will create our own classes(blueprints) to create our own objects.
+
+# to create a class first you need to use the keyword-class followed by the class-name that is PascalCased.
+# Quick little knowledge
+# PascalCase: every new starting word would be capital, e.g: CarBlueprintDemo
+# camelCase: Every new starting word would be capital only except the first word e.g: carBlueprintDemo
+# snake_case: Every word is lower case but separated by the underscore_ e.g: car_blueprint_demo
+
+
+# class-name always start with the capital letter
+class User:
+    # pass can be used to replace the empty space and to avoid any errors
+    pass
+
+
+user_1 = User()
+
+print(user_1)
